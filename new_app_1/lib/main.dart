e487adb5b7f3355cc8f8c1a90723a38e4a672a5b
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-03-26 02:41:54 +0600

    update file

--- new_app_1/lib/main.dart
+++ new_app_1/lib/main.dart
@@ -25,12 +25,84 @@ return MaterialApp(
 
 class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
+  
+  MySnackBar(massage, context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(massage))
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My App"), backgroundColor: Colors.greenAccent,),
-      body: Text("Wellcome to the App"),
+      appBar: AppBar(
+        title: Text("My App"),
+        titleSpacing: 10,
+        // centerTitle: true,
+        toolbarHeight: 60, //Default size= 60
+        toolbarOpacity: 1,
+        elevation: 0,
+        backgroundColor: Colors.greenAccent,
+
+        actions: [
+          IconButton(onPressed: (){MySnackBar("This is Comment Icon", context);}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){MySnackBar("This is Search Icon", context);}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){MySnackBar("This is Settings Icon", context);}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: (){MySnackBar("This is Facebook Icon", context);}, icon: Icon(Icons.facebook)),
+          IconButton(onPressed: (){MySnackBar("This is Email Icon", context);}, icon: Icon(Icons.email)),
+        ],
+      ),
+
+      body: (
+        Container(
+          height: 100,
+          color: Colors.lightBlueAccent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.call, size: 50,),
+                  Text("Call")
+                ],
+              ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.route, size: 50,),
+                  Text("Route")
+                ],
+              ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.share, size: 50,),
+                  Text("Share")
+                ],
+              ),
+              
+            ],
+          ),
+
+        )
+      ),
+
+
+
+      floatingActionButton: FloatingActionButton(
+        elevation: 0,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.greenAccent,
+        onPressed: (){
+          MySnackBar("This is Floating Action Button", context);
+        },
+      ),
 
     );
   }
