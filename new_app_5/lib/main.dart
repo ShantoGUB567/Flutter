e747b3a9a7627cb7c9be3e348854d497a3cbefb7
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-08-09 01:56:12 +0600

    update file

--- new_app_5/lib/main.dart
+++ new_app_5/lib/main.dart
@@ -22,12 +22,53 @@ class HomeActivity extends StatelessWidget{
             appBar: AppBar(title: Text("My App"),),
             body: Column(
               children: [
-                Text("data"),
+                Text(
+                  "data. This text is centered.",
+                  textAlign: TextAlign.center,
+                  textScaleFactor: 1.5,
+                ),
+                Text(
+                  'This is a very long text. This is a very long text. This is a very long text. This is a very long text. This is a very long text. This is a very long text.',
+                  maxLines: 1,
+                  overflow: TextOverflow.fade,
 
+                ),
+                Text(
+                  'Wrapping Text',
+                  softWrap: false,
+                  strutStyle: StrutStyle(
+                    fontSize: 20,
+                    height: 2,),
+                ),
+                Text(
+                  'Right to Left',
+                  textDirection: TextDirection.ltr,
+                ),
+                Text(
+                  '💖',
+                  semanticsLabel: 'Heart Emoji',
+                ),
+                Text(
+                  'Selectable Text', 
+                  selectionColor: Colors.pink,
+                ),
+                Text(
+                  'Stylish Text aro onek kico likhte hobe',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 2.0,
+                    wordSpacing: 4.0,
+                    backgroundColor: Colors.yellow,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.red,
+                    decorationStyle: TextDecorationStyle.dotted,
+                  ),
+                ),
               ],
             ),
           );
     }
   }
-  
-}
