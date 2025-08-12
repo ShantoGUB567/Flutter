580de8ce01711acbd0de10da5f0aee28770aaa0b
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-08-13 00:31:34 +0600

    update main file

--- new_app_5/lib/main.dart
+++ new_app_5/lib/main.dart
@@ -19,56 +19,109 @@ class HomeActivity extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
       return Scaffold(
-            appBar: AppBar(title: Text("My App"),),
-            body: Column(
+            appBar: AppBar(
+              title: Text("My App"),
+              backgroundColor: Colors.greenAccent,
+            ),
+            body:
+            // Column(
+            //   children: [
+            //     Text(
+            //       "data. This text is centered.",
+            //       textAlign: TextAlign.center,
+            //       textScaleFactor: 1.5,
+            //     ),
+            //     Text(
+            //       'This is a very long text. This is a very long text. This is a very long text. This is a very long text. This is a very long text. This is a very long text.',
+            //       maxLines: 1,
+            //       overflow: TextOverflow.fade,
+            //
+            //     ),
+            //     Text(
+            //       'Wrapping Text',
+            //       softWrap: false,
+            //       strutStyle: StrutStyle(
+            //         fontSize: 20,
+            //         height: 2,),
+            //     ),
+            //     Text(
+            //       'Right to Left',
+            //       textDirection: TextDirection.ltr,
+            //     ),
+            //     Text(
+            //       '💖',
+            //       semanticsLabel: 'Heart Emoji',
+            //     ),
+            //     Text(
+            //       'Selectable Text',
+            //       selectionColor: Colors.pink,
+            //     ),
+            //     Text(
+            //       'Stylish Text aro onek kico likhte hobe',
+            //       style: TextStyle(
+            //         color: Colors.blue,
+            //         fontSize: 24,
+            //         fontWeight: FontWeight.bold,
+            //         fontStyle: FontStyle.italic,
+            //         letterSpacing: 2.0,
+            //         wordSpacing: 4.0,
+            //         backgroundColor: Colors.yellow,
+            //         decoration: TextDecoration.underline,
+            //         decorationColor: Colors.red,
+            //         decorationStyle: TextDecorationStyle.dotted,
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            Column(
               children: [
-                Text(
-                  "data. This text is centered.",
-                  textAlign: TextAlign.center,
-                  textScaleFactor: 1.5,
+                // Container(
+                //   height: 50,
+                //   width: 100,
+                //   alignment: Alignment.center,
+                //   // color: Colors.redAccent,
+                //   margin: EdgeInsets.all(10),
+                //   padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                //   decoration: BoxDecoration(
+                //     color: Colors.blueGrey,
+                //     border: Border.all(color: Colors.green, width: 3),
+                //     borderRadius: BorderRadius.circular(10),
+                //     gradient: RadialGradient(colors: [Colors.cyan, Colors.green]),
+                //   ),
+                //   child: Text("Shanto",),
+                // ),
+                Container(child: Text(
+                  "Al Shahriar",
+                  style: TextStyle(fontSize: 20),
                 ),
-                Text(
-                  'This is a very long text. This is a very long text. This is a very long text. This is a very long text. This is a very long text. This is a very long text.',
-                  maxLines: 1,
-                  overflow: TextOverflow.fade,
-
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellowAccent,
                 ),
-                Text(
-                  'Wrapping Text',
-                  softWrap: false,
-                  strutStyle: StrutStyle(
-                    fontSize: 20,
-                    height: 2,),
+                Container(child: Text(
+                  "Ahommed",
+                  style: TextStyle(fontSize: 20),
                 ),
-                Text(
-                  'Right to Left',
-                  textDirection: TextDirection.ltr,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  color: Colors.greenAccent,
                 ),
-                Text(
-                  '💖',
-                  semanticsLabel: 'Heart Emoji',
+                Container(child: Text(
+                  "Shanto",
+                  style: TextStyle(fontSize: 20),
                 ),
-                Text(
-                  'Selectable Text', 
-                  selectionColor: Colors.pink,
-                ),
-                Text(
-                  'Stylish Text aro onek kico likhte hobe',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    letterSpacing: 2.0,
-                    wordSpacing: 4.0,
-                    backgroundColor: Colors.yellow,
-                    decoration: TextDecoration.underline,
-                    decorationColor: Colors.red,
-                    decorationStyle: TextDecorationStyle.dotted,
-                  ),
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orangeAccent,
                 ),
               ],
-            ),
+
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              // verticalDirection: VerticalDirection.up,
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+            )
+
           );
     }
   }
