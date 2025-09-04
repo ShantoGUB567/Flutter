1d69b633de82b2f26ea2646b8c3d22dddcd43beb
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-09-05 01:37:44 +0600

    add new project file

---
+++ NewProject/android/app/src/main/java/com/shantoapp/newproject/MainActivity.java
@@ -0,0 +1,6 @@
+package com.shantoapp.newproject;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ test_p/android/app/src/main/java/com/shanto/test_p/MainActivity.java
@@ -0,0 +1,6 @@
+package com.shanto.test_p;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
