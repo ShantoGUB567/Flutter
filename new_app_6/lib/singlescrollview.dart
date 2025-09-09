a4d979ce2044bb1df484eb15afb1494556355e4e
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-09-10 00:30:19 +0600

    add something

--- new_app_6/lib/singlescrollview.dart
+++ new_app_6/lib/singlescrollview.dart
@@ -8,10 +8,27 @@ class ScrollViewActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("SingleChildScrollView & Padding & Placeholder"),),
-      body: Column(
-        children: [
-          
-        ],
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        // reverse: true,
+        physics: PageScrollPhysics(),
+        controller: _controller,
+        child: Column(
+          children: [
+            Container(
+              height: 500,
+              width: 400,
+              color: Colors.blue[300],
+              margin: EdgeInsets.all(20),
+            ),
+            Container(
+              height: 500,
+              width: 400,
+              color: Colors.green[300],
+              margin: EdgeInsets.all(20),
+            ),
+          ],
+        ),
       ),
     );
   }
