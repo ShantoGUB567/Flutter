d30dfb8fdd72c480530d31b11afa6dd985f89405
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-09-12 23:43:31 +0600

    complete appbar widget

--- new_app_6/lib/appbar.dart
+++ new_app_6/lib/appbar.dart
@@ -8,14 +8,24 @@ class App_Bar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("GUCC"),
+        title: Text("GUCC", style: TextStyle(color: Colors.greenAccent, fontWeight: FontWeight.w800),),
         centerTitle: true,
-        leading: Icon(Icons.menu),
+        // leading: Icon(Icons.menu),
         actions: [
           Icon(Icons.search),
           Icon(Icons.shopping_cart),
         ],
         backgroundColor: Colors.teal,
+        elevation: 10,
+        shadowColor: Colors.redAccent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        // flexibleSpace: Image.network("https://gucc.green.edu.bd/_next/image?url=%2Fevents%2F53.jpg&w=3840&q=75", fit: BoxFit.cover,),
+        toolbarHeight: 50,
+        toolbarTextStyle: TextStyle(color: Colors.white),
+        iconTheme: IconThemeData(color: Colors.white, size: 40),
+        automaticallyImplyLeading: true,
       ),
     );
   }
