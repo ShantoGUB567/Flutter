2de91f1b5fa0c9d08fcb22b88f22f23ea99b2694
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-09-17 23:31:07 +0600

    add new file

--- new_app_6/lib/indexedStack.dart
+++ new_app_6/lib/indexedStack.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class IndexedStack extends StatelessWidget {
   const IndexedStack({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold();
   }
 }
