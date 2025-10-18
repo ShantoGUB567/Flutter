b5cbda03b059af7ac834bbbab13100cf09d2da4e
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-10-18 23:52:34 +0600

    update call features

--- whatsapp_ui/lib/home_activity.dart
+++ whatsapp_ui/lib/home_activity.dart
@@ -64,14 +64,21 @@ class _HomeActivityState extends State<HomeActivity> {
                   subtitle: Text("Hi!!  \nWho are you bro??"),
                   leading: CircleAvatar(child: Icon(Icons.person_4, size: 30,),),
                   trailing: Text("03:47 am"),
-
-
-                  
                 );
               }
             ),
             Text("Status"),
-            Text("Calls"),
+            ListView.builder(
+                itemCount: 15,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text("Shahriar Shanto"),
+                    subtitle: Text("Hi!!  \nWho are you bro??"),
+                    leading: CircleAvatar(child: Icon(Icons.person_4, size: 30,),),
+                    trailing: Text("03:47 am"),
+                  );
+                }
+            ),
           ],
         ),
       ),
